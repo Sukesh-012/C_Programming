5c866f5971234f1c99a41041b6867a32ad570f80
Sukesh-012 <123532254+Sukesh-012@users.noreply.github.com>
2023-02-21 13:54:43 +0530

    Create 54.c

---
+++ 54.c
@@ -0,0 +1,11 @@
+#includ<stdio.h>
+int main()
+{
+int fact=1,i;
+printf("enter the number to find factotial");
+scanf("%d",n);
+for(i=1,i<=n,i++)
+fact=fact*i;
+printf("fctorial of %d is: %d",fact,n);
+return 0;
+}
